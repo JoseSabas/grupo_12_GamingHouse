9e3d18da65af2bbdfbc07faed5c7358b2979033a
obelisk02 <70256661+obelisk02@users.noreply.github.com>
2021-08-19 05:00:11 -0500

    Detalle product
    0

--- GamingHouseApp/app.js
+++ GamingHouseApp/app.js
@@ -3,16 +3,20 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Server running at port 3000 ');
 });
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
-app.get('/carrito', (req,res)=>{
+app.get('/carrito', (req, res) => {
     res.sendFile(__dirname + '/views/productCart.html');
 });
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/detalles', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetails.html');
 });
