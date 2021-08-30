a177abeed7aa7c7ed0d881bbf3cad3ddaf1215b0
obelisk02 <70256661+obelisk02@users.noreply.github.com>
2021-08-29 19:18:55 -0500

    partials,rutas, add-edit product
    Se agrega partials header,footer
    Se agrega routes
    .html a .ejs
    Agrega nuevas vistas
    -newProduct.ejs
    -editProduct.ejs
    -respectivo.css

--- GamingHouseApp/controllers/routesController.js
+++ GamingHouseApp/controllers/routesController.js
@@ -9,13 +9,21 @@ const controlador = {
         res.sendFile(path.join(__dirname, '../views/login.html'));
     },
     productCart: (req, res) => {
-        res.sendFile(path.join(__dirname, '../views/productCart.html'));
+        res.render("productCart");
     },
     productDetail: (req, res) => {
-        res.sendFile(path.join(__dirname, '../views/productDetails.html'));
+        res.render("productDetails");
     },
     register: (req, res) => {
         res.sendFile(path.join(__dirname, '../views/register.html'));
+    },
+
+    newProduct: (req, res) => {
+        res.render("newProduct");
+    },
+
+    editProduct: (req, res) => {
+        res.render("editProduct");
     }
 }
 
